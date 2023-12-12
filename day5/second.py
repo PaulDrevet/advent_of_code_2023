dfba1f434e4cb3a5e2ff9b47f40b1ab0d49f24df
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2023-12-12 09:37:16 +0100

    ????

--- day5/second.py
+++ day5/second.py
@@ -6,6 +6,19 @@ a = {}
 res = 10000000000000000000000000000000
 seeds2 =[]
 
+def seedToLocation(j):
+    res = 1000000000000000000000000000000000000000000000000000000000000000000000000
+    for m in a:
+        for mapLine in a[m]:
+            if int(mapLine[1]) <= j <= int(mapLine[1])+int(mapLine[2]):
+                j = int(mapLine[0]) + (j - int(mapLine[1]))
+                break
+        if m == "humidity-to-location map:":
+            if j < res:
+                res = j
+    return res
+
+
 seeds = lines[0].split(":")[1].strip().split(" ")
 for i in range(1, len(lines)):
     if lines[i][0].isalpha():
@@ -17,25 +30,34 @@ for i in range(1, len(lines)):
             a[key] += [aLine]
             i += 1
 
-def seedToLocation(seed, length):
-    res = 1000000000000000000000000000000000000000000000000000000000000000000000000
-    print("Thread started" + str(seed))
-    for j in range(seed, seed+length):
-        for m in a:
-            for mapLine in a[m]:
-                if int(mapLine[1]) <= j <= int(mapLine[1])+int(mapLine[2]):
-                    j = int(mapLine[0]) + (j - int(mapLine[1]))
-                    break
-            if m == "humidity-to-location map:":
-                if j < res:
-                    res = j
-    print(res)
 
 
+min = 1000000000000000000000000000000000000000000000000000000000000000000
+bigMin = 1000000000000000000000000000000000000000000000000000000000000000000
 for i in range(0, len(seeds), 2):
+    inter = 12
+
     seed = int(seeds[i])
     length = int(seeds[i+1])
-    threading.Thread(target=seedToLocation, args=(seed, length)).start()
+    for j in range(seed, seed+length, inter):
+        res = seedToLocation(seed+j)
+        if res < min:
+            min = res
+            minInterval = (seed+j-inter, seed+j)
+            print(minInterval)
+            print(min, "\n")
+    if min < bigMin:
+        bigMin = min
+print(minInterval)
+print(bigMin)
+
+for i in range(minInterval[0], minInterval[1]):
+    res = seedToLocation(i)
+    if res < bigMin:
+        bigMin = res
+print(bigMin)
+
+
 
 
 
