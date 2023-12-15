a3d50ac6300fd5407a4ea9009c2c2ba259304120
PaulDrevet <94627523+PaulDrevet@users.noreply.github.com>
2023-12-15 14:36:36 +0100

    day 13 rq

--- day13/main.py
+++ day13/main.py
@@ -7,10 +7,8 @@ def splitLines(text):
     for i in range(0, len(text)-1):
         if text[i] == '\n':
             res += [text[:i+j]]
-            print(text[:i])
             j = i
     res += [text[j+1:]]
-    print(res)
     return res
 
 
@@ -19,22 +17,29 @@ def reformat(pattern):
         pattern[i] = pattern[i].rstrip()
     return pattern
 
+
+
 def turn(pattern):
-    newPattern = list(zip(*pattern[::-1]))
-    return ["".join(x) for x in newPattern]
-
-
-
+    newP = []
+    print(pattern)
+    for i in range(len(pattern[0])):
+        a = ""
+        for j in range(len(pattern)):
+            if pattern[j] != "":
+                a += pattern[j][i]
+        newP.append(a)
+    return newP
 
 
 def getReflection(pattern):
-    pattern = reformat(pattern)
+    print(pattern)
     for i in range(1, len(pattern)):
         if i <= len(pattern)//2:
             p1 = pattern[:i]
             p2 = pattern[i:2*i]
             if p1 == p2:
                 print("reflection")
+                return i
 
     pattern.reverse()
     for i in range(1, len(pattern)):
@@ -44,22 +49,25 @@ def getReflection(pattern):
             p2.reverse()
             if p1 == p2:
                 print("reflection", len(pattern)-i)
-
-
-
-    print("\n\n")
-
-
+                return len(pattern)-i
+    return 0
 
 
 def solve():
+    col = []
+    row = []
+    s = 0
     patterns = splitLines(lines)
-    print(patterns)
     for p in patterns:
-        getReflection(p)
-        newP = turn(p)
-    for a in newP:
-        print(a)
+        p = reformat(p)
+
+        col.append(getReflection(p))
+        row.append(getReflection(turn(p)))
+        print("\n")
+    print(col)
+    s += sum(col * 100)
+    s += sum(row)
+    print(s)
 
 
 solve()
